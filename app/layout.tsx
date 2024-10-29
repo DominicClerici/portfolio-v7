7a0e74a0cd98072b3605c30f1fe1f3cab70ec74d
dclerici <dclerici77@gmail.com>
2024-10-29 14:01:51 -0400

    updated metadata baseurl

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import "./globals.css"
 import Footer from "./_home/Footer"
 import Header from "./_home/Header"
 
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
+
 export const metadata = {
   title: "Dominic Clerici",
   description:
     "The professional developer portfolio of Dominic Clerici, A computer science student at Indiana University.",
   keywords: ["Dominic", "Clerici", "Portfolio", "Web Developer", "React", "JavaScript"],
-  metadataBase: new URL("https://www.dominicclerici.com"),
+  metadataBase: new URL(defaultUrl),
 }
 const _inter = localFont({
   src: "./fonts/InterVariable.ttf",
